b9f5d8990bfc60c9d0845eac76e1af2f179906eb
mayu.ogata <mayu.ogata@rakuten.com>
2018-06-14 01:34:24 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -29,7 +29,14 @@ class TasksController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+         $this->validate($request, [
+            'status' => 'required|max:255',
+            'content' => 'required|max:255',
+        ]);
+
+
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -58,7 +65,13 @@ class TasksController extends Controller
     // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
    public function update(Request $request, $id)
     {
+         $this->validate($request, [
+            'status' => 'required|max:255', 
+            'content' => 'required|max:255',
+        ]);
+
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- database/migrations/2018_06_14_095313_add_status_table.php
+++ database/migrations/2018_06_14_095313_add_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class AddStatusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-         Schema::create('messages', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content');    // content カラム追加
+            $table->string('content');
+            $table->string('stattus');
             $table->timestamps();
+            
         });
-
     }
 
     /**
